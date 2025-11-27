c001f29bdc951657e82c5c02db3dbc170e256b20
24DP4MBruv <A240827MB@rvt.lv>
2025-11-27 09:26:50 +0200

    Numbers

--- src/main/java/rvt/numbers.java
+++ src/main/java/rvt/numbers.java
@@ -3,12 +3,23 @@ package rvt;
 import java.util.Scanner;
 
 public class numbers {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ievadiet pirmo skaitli: ");
-        double nr1 = scanner.nextDouble();
-        System.out.println("Ievadiet otro skaitli: ");
-        double nr2 = scanner.nextDouble();
-        System.out.println(nr1 + nr2  );
+
+        System.out.print("Give the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Give the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Give the third number: ");
+        int num3 = scanner.nextInt();
+
+        double average = (num1 + num2 + num3) / 3.0;
+
+        System.out.println("The average is " + average);
+
+        scanner.close();
     }
 }
+
